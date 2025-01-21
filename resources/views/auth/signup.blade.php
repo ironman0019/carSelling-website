dde1d8686afa646c506e84deeeea13ff6ff9d2b2
Emad Isavi <emadesavi@gmail.com>
2025-01-21 18:01:07 +0330

    Create signup page

---
+++ resources/views/auth/signup.blade.php
@@ -0,0 +1,33 @@
+<x-guest-layout title="Signup" bodyClass="page-signup">
+
+    <form action="" method="post">
+        <div class="form-group">
+            <input type="email" placeholder="Your Email" />
+        </div>
+        <div class="form-group">
+            <input type="password" placeholder="Your Password" />
+        </div>
+        <div class="form-group">
+            <input type="password" placeholder="Repeat Password" />
+        </div>
+        <hr />
+        <div class="form-group">
+            <input type="text" placeholder="First Name" />
+        </div>
+        <div class="form-group">
+            <input type="text" placeholder="Last Name" />
+        </div>
+        <div class="form-group">
+            <input type="text" placeholder="Phone" />
+        </div>
+        <button class="btn btn-primary btn-login w-full">Register</button>
+    </form>
+
+    <x-slot:clickHere>
+        <div class="login-text-dont-have-account">
+            Already have an account? -
+            <a href="/login.html"> Click here to login </a>
+        </div>
+    </x-slot:clickHere>
+
+</x-guest-layout>
