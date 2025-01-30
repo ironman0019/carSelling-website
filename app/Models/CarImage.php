bafeeb72f8edf076d2e51da6666f072b2ea1cad4
Emad Isavi <emadesavi@gmail.com>
2025-01-30 18:45:07 +0330

    Add relation with car in car image model

---
+++ app/Models/CarImage.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class CarImage extends Model
+{
+
+    public $timestamps = false;
+    
+
+    public function car(): BelongsTo
+    {
+        return $this->belongsTo(Car::class);
+    }
+    
+}
