ca595f35685623d761b87c3bc57dfff175f8814d
Emad Isavi <emadesavi@gmail.com>
2025-01-30 19:00:36 +0330

    Add relation with cars and cities in state model

--- app/Models/State.php
+++ app/Models/State.php
@@ -3,10 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class State extends Model
 {
     
     public $timestamps = false;
+
+
+    public function cars(): HasMany
+    {
+        return $this->hasMany(Car::class);
+    }
+
+
+    public function cities(): HasMany
+    {
+        return $this->hasMany(City::class);
+    }
     
 }
