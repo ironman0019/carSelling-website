4c13107371b175981c23cd545b8abc9241bb25db
Emad Isavi <emadesavi@gmail.com>
2025-01-30 18:46:31 +0330

    Add relation with car and set primary key to car_id in car features model

--- app/Models/CarFeatures.php
+++ app/Models/CarFeatures.php
@@ -3,10 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class CarFeatures extends Model
 {
     
     public $timestamps = false;
+
+    protected $primaryKey = 'car_id';
+    
+
+    public function car(): BelongsTo
+    {
+        return $this->belongsTo(Car::class);
+    }
 
 }
