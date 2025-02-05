41c3f2d1330a07398914af8380c3204d6efdfdea
Emad Isavi <emadesavi@gmail.com>
2025-02-05 22:27:50 +0330

    Add eager loading in cars query in index function

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,7 +9,7 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $cars = Car::where('published_at', '<', now())->orderBy('published_at', 'desc')->limit(30)->get();
+        $cars = Car::with(['primaryImage', 'city', 'maker', 'model', 'carType', 'fuelType'])->where('published_at', '<', now())->orderBy('published_at', 'desc')->limit(30)->get();
         return view('index', compact('cars'));
     }
 }
